34bb09655ac3bcd54a661ddcbcc45389933e36f4
ebolyen <ebolyen@gmail.com>
2015-04-14 15:40:19 -0700

    The beginning of a poster

--- assets/py/convert_fastq.py
+++ assets/py/convert_fastq.py
@@ -1,8 +1,4 @@
 from skbio.io import read, write
 
-seqs = read("example.fna", qual="example.qual",
-                           format="fasta")
-
-write(seqs, into="example.fastq",
-            variant="illumina1.8",
-            format="fastq")
+seqs = read("example.fna", qual="example.qual", format="fasta")
+write(seqs, into="example.fastq", variant="illumina1.8", format="fastq")
